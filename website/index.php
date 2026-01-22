3c4e055b566ea312012381f7c30ae4ff82d5ecfc
Alek <92643775+barealek@users.noreply.github.com>
2026-01-22 15:22:37 +0100

    refresh side hver 3 sek

--- website/index.php
+++ website/index.php
@@ -12,3 +12,9 @@ while ($row = mysqli_fetch_assoc($result)) {
                 : "Væk");
     echo $row["name"] . ": " . $row["uuid"] . " - " . $state . "<br>";
 }
+
+<script>
+setTimeout(() => {
+window.reload();
+}, 3000)
+</script>
