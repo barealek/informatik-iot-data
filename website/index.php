dfcb029bf6aab5351ce9fe4af43abaaaf154532a
Alek <92643775+barealek@users.noreply.github.com>
2026-02-02 13:31:40 +0100

    rettelser i php

--- website/index.php
+++ website/index.php
@@ -2,6 +2,33 @@
 include_once "db.php";
 DB_Connect("iot_opsamling");
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"] ?? "";
+    $uuid = $_POST["uuid"] ?? "";
+
+    if ($name && $uuid) {
+        $ch = curl_init("http://server:8080/devices");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(["name" => $name, "uuid" => $uuid]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+        curl_exec($ch);
+        curl_close($ch);
+        header("Location: index.php");
+        exit;
+    }
+}
+
+?>
+
+<form method="POST">
+    <input type="text" name="name" placeholder="Navn" required>
+    <input type="text" name="uuid" placeholder="UUID" required>
+    <button type="submit">Tilføj enhed</button>
+</form>
+
+
+<div id="devices">
+<?php
 $result = mysqli_query($forbindelse, "SELECT * FROM devices");
 while ($row = mysqli_fetch_assoc($result)) {
     $state =
@@ -13,10 +40,6 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo $row["name"] . ": " . $row["uuid"] . " - " . $state . "<br>";
 }
 ?>
+</div>
 
-
-<script>
-setTimeout(() => {
-location.reload();
-}, 3000)
-</script>
+<button onclick="location.reload()">Opdater</button>
